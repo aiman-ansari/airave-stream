0cb3aed5cc6326255878379c75c54926c3167342
Aiman <ansariaiman2000@gmail.com>
2022-05-19 06:02:48 -0700

    Added category context

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom"
+import { CategoryProvider } from "./Context/CategoryContext";
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+    <CategoryProvider>
+      <App />
+    </CategoryProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
