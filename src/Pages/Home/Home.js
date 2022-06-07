1510936ed602daf4521bc1b7b49200c67fdcac88
Aiman <ansariaiman2000@gmail.com>
2022-06-06 21:24:47 -0700

    Update Home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,6 +9,7 @@ export const Home = () =>{
     return(
         <>
             <div className='container'>
+                <span>checking</span>
             <div className='image'>
                 <iframe 
                 className='img'
