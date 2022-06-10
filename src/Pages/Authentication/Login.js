044a2a724b760320c8cf2172a526274b259b0645
Aiman <ansariaiman2000@gmail.com>
2022-06-10 04:29:41 -0700

    fix: minor changes

--- src/Pages/Authentication/Login.js
+++ src/Pages/Authentication/Login.js
@@ -13,8 +13,8 @@ export const Login = () => {
   const [error, setError] = useState(false);
   const [message, setMessage] = useState("");
   const test = {
-    email: "testuser@gmail.com",
-    password: "testuser123",
+    email: "adarshbalika@gmail.com",
+    password: "adarshBalika123",
   };
   const handleSubmit = () => {
     if (email && password) {
